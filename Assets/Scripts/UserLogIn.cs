ee523e554f6fd97d7db5509e81157d90dff14952
RECNEPS-KIPS <2432719656@qq.com>
2020-05-15 20:09:15 +0800

    登录注册界面功能完善

--- Assets/Scripts/UserLogIn.cs
+++ Assets/Scripts/UserLogIn.cs
@@ -4,8 +4,7 @@ using System.Data;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class UserLogIn : MonoBehaviour
-{
+public class UserLogIn : MonoBehaviour {
     public InputField userNameInput;
     public InputField passwordInput;
     public Image mes;
@@ -15,43 +14,59 @@ public class UserLogIn : MonoBehaviour
     public string password;//密码
     public string databaseName;//数据库名称
     protected MySqlAccess mysql;//封装好的数据库类
+
+    public GameObject error;
+    public Text errorText;
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start() {
         mysql = new MySqlAccess(host, port, userName, password, databaseName);
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+
     }
 
     public void OnLogInBtnClick() {
-        mysql.OpenSql();
-        //获取数据
-        DataSet ds = mysql.Select("users", new string[] { "user_password","user_id" }, new string[] { "user_name"}, new string[] { "="}, new string[] { userNameInput.text,});
-        if (ds != null) {
-            DataTable table = ds.Tables[0];
-            List<string>data=new List<string>();
-            foreach (DataRow row in table.Rows) {
-                foreach (DataColumn col in table.Columns) {
-                    data.Add(row[col].ToString());
-                }
-            }
-            if (data[0]==passwordInput.text) {
-                Debug.Log("登陆成功");
-                mes.color = Color.green;
-            }
-            else {
-                Debug.Log("登陆失败");
-                mes.color = Color.red;
-            }
+        if (userNameInput.text == "") {
+            Message("请输入用户名");
+        }
+        else if (passwordInput.text == "") {
+            Message("请输入密码");
         }
         else {
-            Debug.Log("登陆失败");
-            mes.color = Color.red;
+            mysql.OpenSql();
+            //获取数据
+            DataSet ds = mysql.Select("users", new string[] { "user_password", "user_id" }, new string[] { "user_name" }, new string[] { "=" }, new string[] { userNameInput.text, });
+            if (ds != null) {
+                DataTable table = ds.Tables[0];
+                List<string> data = new List<string>();
+                foreach (DataRow row in table.Rows) {
+                    foreach (DataColumn col in table.Columns) {
+                        data.Add(row[col].ToString());
+                    }
+                }
+                if (data[0] == passwordInput.text) {
+                    mes.color = Color.green;
+                    Message("登录成功");
+                }
+                else {
+                    mes.color = Color.red;
+                    Message("登陆失败");
+                }
+            }
+            else {
+                mes.color = Color.red;
+                Message("登陆失败");
+            }
+            mysql.CloseSql();
         }
-        mysql.CloseSql();
+
+    }
+
+    public void Message(string mes) {
+        error.GetComponent<Animator>().SetTrigger("trigger");
+        errorText.GetComponent<Text>().text = mes;
+        errorText.GetComponent<Text>().color = new Color(255, 255, 255, 255);
     }
 }
--- Assets/Scripts/UserSignUp.cs
+++ Assets/Scripts/UserSignUp.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Data;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UserSignUp : UserLogIn {
     // Start is called before the first frame update
@@ -15,9 +16,23 @@ public class UserSignUp : UserLogIn {
     }
 
     public void OnSignUpBtnClick() {
-        mysql.OpenSql();
-        //插入数据
-        bool succ = mysql.Insert("users", new[] { "user_name", "user_password" }, new[] { userNameInput.text, passwordInput.text });
-        Debug.Log(succ ? "注册成功,请重新登录" : "注册失败,用户名已存在,请重新注册");
+        if (userNameInput.text == "") {
+            Message("用户名不能为空");
+        }
+        else if (passwordInput.text == "") {
+            Message("密码不能为空");
+        }
+        else {
+            mysql.OpenSql();
+            //插入数据
+            bool succ = mysql.Insert("users", new[] { "user_name", "user_password" }, new[] { userNameInput.text, passwordInput.text });
+            Debug.Log(succ ? "注册成功,请重新登录" : "注册失败,用户名已存在,请重新注册");
+            if (succ) {
+                Message("注册成功,请登录");
+            }
+            else {
+                Message("注册失败,请重新注册");
+            }
+        }
     }
 }
